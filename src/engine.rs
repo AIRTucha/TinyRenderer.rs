72528fb0bc2be7651297f07fe3e039af4803c84c
AIRTucha <airtucha@icloud.com>
2019-11-25 21:37:53 +0100

    Emplementation of initial engine

--- src/engine.rs
+++ src/engine.rs
@@ -17,14 +17,14 @@ pub struct Engine {
 }
 
 impl Engine {
-    pub fn render(&self, img: &mut Scene) -> &self::Engine {
-        self.context.put_image_data(&img.image(), 0.0, 0.0);
+    pub fn render(&self, scene: &web_sys::ImageData) -> &self::Engine {
+        self.context.put_image_data(scene, 0.0, 0.0);
         self
     }
-    pub fn new(id: &str) -> Engine {
+    pub fn new(id: String) -> Engine {
         let document = web_sys::window().unwrap().document().unwrap();
         let canvas: web_sys::HtmlCanvasElement = document
-            .get_element_by_id(id)
+            .get_element_by_id("canvas")
             .unwrap()
             .dyn_into::<web_sys::HtmlCanvasElement>()
             .map_err(|_| ())
@@ -38,59 +38,9 @@ impl Engine {
         Engine {
             width: canvas.width(),
             height: canvas.height(),
-            id: String::from(id),
+            id: id,
             canvas: canvas,
             context: context,
-        }
-    }
-}
-
-pub struct Scene {
-    width: usize,
-    height: usize,
-    pixel_count: usize,
-    data_size: usize,
-    image: Vec<u8>,
-    z_buffer: Vec<Vec<f32>>,
-}
-
-impl Scene {
-    pub fn dot(&mut self, x: usize, y: usize, z: f32, r: u8, g: u8, b: u8, a: u8) {
-        let red_index = (self.width * y + x) * 4;
-        self.image[red_index] = r;
-        self.image[red_index + 1] = g;
-        self.image[red_index + 2] = b;
-        self.image[red_index + 3] = a;
-        self.z_buffer[x][y] = z;
-    }
-    pub fn image(&mut self) -> ImageData {
-        ImageData::new_with_u8_clamped_array_and_sh(
-            Clamped(&mut self.image),
-            self.width as u32,
-            self.height as u32,
-        )
-        .unwrap()
-    }
-    pub fn clear(&mut self) {
-        let mut i = 0;
-        while i < self.data_size {
-            self.image[i] = 0;
-            self.image[i + 1] = 0;
-            self.image[i + 2] = 0;
-            self.image[i + 3] = 255;
-            i += 4;
-        }
-    }
-    pub fn new(width: usize, height: usize) -> Scene {
-        let pixel_count = width * height;
-        let data_size = pixel_count * 4;
-        Scene {
-            width: width,
-            height: height,
-            pixel_count: pixel_count,
-            data_size: data_size,
-            image: vec![0; data_size],
-            z_buffer: vec![vec![-100.0; height]; width],
         }
     }
 }
--- src/lib.rs
+++ src/lib.rs
@@ -1,16 +1,11 @@
 mod engine;
-
 use engine::Engine;
-use engine::Scene;
 use std::f64;
 use wasm_bindgen::prelude::*;
 use wasm_bindgen::Clamped;
 use wasm_bindgen::JsCast;
 use web_sys::ImageData;
 use web_sys::*;
-
-use std::iter::FromIterator;
-
 // When the `wee_alloc` feature is enabled, this uses `wee_alloc` as the global
 // allocator.
 //
@@ -22,43 +17,9 @@ static ALLOC: wee_alloc::WeeAlloc = wee_alloc::WeeAlloc::INIT;
 // This is like the `main` function, except for JavaScript.
 #[wasm_bindgen(start)]
 pub fn main_js() -> Result<(), JsValue> {
-    let engine = Engine::new("canvas");
-    let mut scene = Scene::new(100, 100);
-    scene.clear();
-    // for x in 5..50 {
-    //     scene.dot(x, 50, 0.0, 255, 0, 0, 255)
-    // }
-    diamond(100, &mut scene);
-    engine.render(&mut scene);
+    let engine = Engine::new("canvas".to_string());
+
+    let image = ImageData::new_with_u8_clamped_array_and_sh(Clamped(&mut [100; 40000]), 100, 100)?;
+    engine.render(&image);
     Ok(())
-}
-
-fn diamond(size: usize, scene: &mut Scene) {
-    for row in 0..size {
-        diamond_row(row, size, scene);
-    }
-}
-
-fn diamond_row(position: usize, size: usize, scene: &mut Scene) {
-    let mut counter = 0;
-    let hsize = size / 2;
-    let index = if hsize >= position {
-        hsize - position
-    } else {
-        position - hsize
-    };
-    loop {
-        if counter == size {
-            break;
-        } else if counter >= index && counter < size - index {
-            scene.dot(counter, position, 0.0, 255, 0, 0, 255);
-        }
-        counter += 1;
-    }
-}
-
-fn draw_figure(figure: Vec<Vec<char>>) {
-    figure.iter().for_each(|row| {
-        println!["{}", String::from_iter(row)];
-    });
 }
