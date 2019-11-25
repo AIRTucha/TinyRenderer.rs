6547e12fc674f47bf48c1eef03dbe5538bbd55b8
AIRTucha <airtucha@icloud.com>
2019-11-25 21:37:53 +0100

    Emplementation of initial engine

---
+++ src/engine.rs
@@ -0,0 +1,44 @@
+use std::f64;
+use wasm_bindgen::prelude::*;
+use wasm_bindgen::Clamped;
+use wasm_bindgen::JsCast;
+use web_sys::HtmlCanvasElement;
+use web_sys::ImageData;
+use web_sys::*;
+
+pub struct Engine {
+    width: u32,
+    height: u32,
+    id: String,
+    canvas: web_sys::HtmlCanvasElement,
+    context: web_sys::CanvasRenderingContext2d,
+}
+
+impl Engine {
+    pub fn render(&self, scene: &web_sys::ImageData) -> &self::Engine {
+        self.context.put_image_data(scene, 0.0, 0.0);
+        self
+    }
+    pub fn new(id: String) -> Engine {
+        let document = web_sys::window().unwrap().document().unwrap();
+        let canvas: web_sys::HtmlCanvasElement = document
+            .get_element_by_id("canvas")
+            .unwrap()
+            .dyn_into::<web_sys::HtmlCanvasElement>()
+            .map_err(|_| ())
+            .unwrap();
+        let context = canvas
+            .get_context("2d")
+            .unwrap()
+            .unwrap()
+            .dyn_into::<web_sys::CanvasRenderingContext2d>()
+            .unwrap();
+        Engine {
+            width: canvas.width(),
+            height: canvas.height(),
+            id: id,
+            canvas: canvas,
+            context: context,
+        }
+    }
+}
--- src/lib.rs
+++ src/lib.rs
@@ -1,19 +1,25 @@
-mod utils;
-
+mod engine;
+use engine::Engine;
+use std::f64;
 use wasm_bindgen::prelude::*;
-
-// When the `wee_alloc` feature is enabled, use `wee_alloc` as the global
+use wasm_bindgen::Clamped;
+use wasm_bindgen::JsCast;
+use web_sys::ImageData;
+use web_sys::*;
+// When the `wee_alloc` feature is enabled, this uses `wee_alloc` as the global
 // allocator.
+//
+// If you don't want to use `wee_alloc`, you can safely delete this.
 #[cfg(feature = "wee_alloc")]
 #[global_allocator]
 static ALLOC: wee_alloc::WeeAlloc = wee_alloc::WeeAlloc::INIT;
 
-#[wasm_bindgen]
-extern {
-    fn alert(s: &str);
-}
+// This is like the `main` function, except for JavaScript.
+#[wasm_bindgen(start)]
+pub fn main_js() -> Result<(), JsValue> {
+    let engine = Engine::new("canvas".to_string());
 
-#[wasm_bindgen]
-pub fn greet() {
-    alert("Hello, tinyrenderer!");
+    let image = ImageData::new_with_u8_clamped_array_and_sh(Clamped(&mut [100; 40000]), 100, 100)?;
+    engine.render(&image);
+    Ok(())
 }
